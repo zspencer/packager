b08c8cfe421a43da700b0a92a9fc2cb8fb5760fd
Zee Spencer <zee@zeespencer.com>
2015-01-24 17:36:42 -0800

    Use docker 1.4.1 in the vagrant machine

--- builder_host/scripts/docker.sh
+++ builder_host/scripts/docker.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -eux
 
 echo "Installing Docker"
+# We install 1.3.2 to get the proper systemd/etc
 yum -y install docker-1.3.2
+service docker stop
+
+# And immediately upgrade it to 1.4.1
+wget https://get.docker.com/builds/Linux/x86_64/docker-1.4.1 -O docker
+mv docker /usr/bin/docker
+chmod +x /usr/bin/docker
 
 systemctl enable docker
 service docker start
